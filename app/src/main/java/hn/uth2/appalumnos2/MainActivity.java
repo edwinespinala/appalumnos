984123793700579b62b5c09982ceb934bcfffe4a
edwinespinala <respinal78@gmail.com>
2023-06-05 12:52:30 -0600

    allalumnos_cambios
    cambio de mensaje

--- app/src/main/java/hn/uth2/appalumnos2/MainActivity.java
+++ app/src/main/java/hn/uth2/appalumnos2/MainActivity.java
@@ -3,12 +3,36 @@ package hn.uth2.appalumnos2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import hn.uth2.appalumnos2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.btnSaludar.setOnClickListener(v ->{
+
+             if(binding.edtNombre.getText().toString().isEmpty()){
+            //  Snackbar.make(binding.layaoutBienvenida, R.string.mesnaje_error_nombre,
+             //         Snackbar.LENGTH_LONG).show();
+                 Toast.makeText(getApplicationContext(), "El campo está vacío", Toast.LENGTH_SHORT).show();
+
+             }else {
+
+                 String mensajeBienvenida= getString(R.string.mensaje_bienvenida, binding.edtNombre.getText());
+                 binding.txtTitulo.setText(mensajeBienvenida);
+                 //binding.txtTitulo.setText("Bienvenido" + binding.edtNombre.getText());
+             }
+        });
+
     }
 }
